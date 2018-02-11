1aeec81161b08c4a11fa29aecf348bf0c9949241
Moksh Jain <mokshjn00@gmail.com>
2018-02-11 17:58:19 +0530

    add utils

---
+++ utils/ops.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+
+def squash(s, axis=-1, epsilon=1e-7, name=None):
+    with tf.name_scope(name, default_name="squash"):
+        squared_norm = tf.reduce_sum(tf.square(s), axis=axis,
+                                     keep_dims=True)
+        safe_norm = tf.sqrt(squared_norm + epsilon)
+        squash_factor = squared_norm / (1. + squared_norm)
+        unit_vector = s / safe_norm
+        return squash_factor * unit_vector
+
+
+def safe_norm(s, axis=-1, epsilon=1e-7, keep_dims=False, name=None):
+    with tf.name_scope(name, default_name="safe_norm"):
+        squared_norm = tf.reduce_sum(tf.square(s), axis=axis,
+                                     keep_dims=keep_dims)
+        return tf.sqrt(squared_norm + epsilon)
